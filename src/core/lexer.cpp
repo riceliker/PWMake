93f63c5c0c0ecb6316ff8eca90f59abb89779061
riceliker <108409208+riceliker@users.noreply.github.com>
2026-07-26 22:38:45 +0800

    allow tab and space in the top of line

--- src/core/lexer.cpp
+++ src/core/lexer.cpp
@@ -6,10 +6,15 @@
 
 namespace PWMake::Core
 {    
-    void Lexer::DivideGroup(std::vector<std::string> line, int pc)
+    void Lexer::DivideGroup(std::vector<std::string> lines, int pc)
     {
-        auto [name, params] = Function(line[pc]);
-        auto group = CatchGroup(line, pc);
+        auto line = lines[pc];
+        size_t start = 0;
+        while (start < line.size() && std::isspace(static_cast<unsigned char>(line[start])))
+            start++;
+        line.erase(0, start);
+        auto [name, params] = Function(line);
+        auto group = CatchGroup(lines, pc);
         if (name == "@files")
         {
             
@@ -26,7 +31,7 @@ namespace PWMake::Core
         {
             std::printf("\033[31m" "Error:" "\033[0m" " Unknow Group Name!\n");
             std::printf("In build.pwm:%d\n", pc+1);
-            std::printf("%d | %s\n", pc+1, line[pc].c_str());
+            std::printf("%d | %s\n", pc+1, lines[pc].c_str());
             std::exit(1);
         }
     }
@@ -40,20 +45,25 @@ namespace PWMake::Core
             std::exit(1);
         }
     }
-    void Lexer::RegistryVariable(std::vector<std::string> line, int pc)
+    void Lexer::RegistryVariable(std::vector<std::string> lines, int pc)
     {
-        auto [name, param] = Function(line[pc]);
+        auto line = lines[pc];
+        size_t start = 0;
+        while (start < line.size() && std::isspace(static_cast<unsigned char>(line[start])))
+            start++;
+        line.erase(0, start);
+        auto [name, param] = Function(line);
         auto params = std::move(param);
         if (name == "$string")
         {
-            CheckParam(params, 2, line, pc);
+            CheckParam(params, 2, lines, pc);
             std::string a = GetString(this->string_variable,params->at(1));
             this->string_variable.insert({params->at(0), a});
             return;
         }
         else if (name == "$same") 
         {
-            CheckParam(params, 3, line, pc);
+            CheckParam(params, 3, lines, pc);
             std::string a = GetString(this->string_variable,params->at(1));
             std::string b = GetString(this->string_variable,params->at(2));
             this->bool_variable.insert({params->at(0), a==b});
@@ -61,7 +71,7 @@ namespace PWMake::Core
         }
         else if (name == "$in") 
         {
-            CheckParam(params, 3, line, pc);
+            CheckParam(params, 3, lines, pc);
             std::string a = GetString(this->string_variable,params->at(1));
             std::string b = GetString(this->string_variable,params->at(2));
             this->bool_variable.insert({params->at(0), a.find(b) != std::string::npos});
@@ -70,14 +80,14 @@ namespace PWMake::Core
 
         if (name == "$prints") 
         {
-            CheckParam(params, 1, line, pc);
+            CheckParam(params, 1, lines, pc);
             std::string a = GetString(this->string_variable,params->at(0));
             std::printf("%s\n", a.c_str());
             return;
         }
         else if (name == "$printb")
         {
-            CheckParam(params, 1, line, pc);
+            CheckParam(params, 1, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(0));
             std::printf("%s\n", a?"true":"false");
             return;
@@ -85,21 +95,21 @@ namespace PWMake::Core
         
         if (name == "$bool")
         {
-            CheckParam(params, 2, line, pc);
+            CheckParam(params, 2, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(1));
             this->bool_variable.insert({params->at(0), a});
             return;
         }
         else if (name == "$not") 
         {
-            CheckParam(params, 2, line, pc);
+            CheckParam(params, 2, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(1));
             this->bool_variable.insert({params->at(0), !a});
             return;
         }
         else if (name == "$and") 
         {
-            CheckParam(params, 3, line, pc);
+            CheckParam(params, 3, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(1));
             bool b = GetBool(this->bool_variable, params->at(2));
             this->bool_variable.insert({params->at(0), a&&b});
@@ -107,7 +117,7 @@ namespace PWMake::Core
         }
         else if (name == "$or") 
         {
-            CheckParam(params, 3, line, pc);
+            CheckParam(params, 3, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(1));
             bool b = GetBool(this->bool_variable, params->at(2));
             this->bool_variable.insert({params->at(0), a||b});
@@ -115,16 +125,16 @@ namespace PWMake::Core
         }
         else if(name == "$equal")
         {
-            CheckParam(params, 3, line, pc);
+            CheckParam(params, 3, lines, pc);
             bool a = GetBool(this->bool_variable, params->at(1));
             bool b = GetBool(this->bool_variable, params->at(2));
             this->bool_variable.insert({params->at(0), a==b});
             return;
         }
 
-        std::printf("\033[31m" "Error:" "\033[0m" " The Unknown Type Is Not Support!\n");
+        std::printf("\033[31m" "Error:" "\033[0m" " The Unknown Type Is Not Support! Name:%s\n", name.c_str());
         std::printf("In build.pwm:%d\n", pc+1);
-        std::printf("%d | %s\n", pc+1, line[pc].c_str());
+        std::printf("%d | %s\n", pc+1, lines[pc].c_str());
         std::exit(1);
     }
     Lexer::Lexer(std::vector<std::string> lines)
@@ -133,6 +143,14 @@ namespace PWMake::Core
         while (pc != lines.size() - 1)
         {
             auto line = lines[pc];
+            size_t start = 0;
+            while (start < line.size() && std::isspace(static_cast<unsigned char>(line[start])))
+                start++;
+            line.erase(0, start);
+            if (line.length() == 0)
+            {
+                goto step_next_line;
+            }
             switch (line[0]) 
             {
             case '#':
@@ -159,37 +177,6 @@ namespace PWMake::Core
             step_next_line:
             pc += 1;
         }
-    }
-    std::vector<std::vector<std::string>> DivideGroups(std::vector<std::string> lines)
-    {
-        std::vector<std::vector<std::string>> groups;
-        bool is_group = false;
-        std::vector<std::string> group;
-        for(const auto& line: lines)
-        {
-            // void
-            if (line.length() == 0 && line[0] == '#') 
-                continue;
-            switch (line[0]) 
-            {
-            // The start of group
-            case '@':
-                is_group = true;
-                break;
-            // The end of group
-            case ';':
-                is_group = false;
-                groups.push_back(group);
-                group = std::vector<std::string>();
-                break;
-            }
-            
-            if (is_group == true)
-            {
-                group.push_back(line);
-            }
-        }
-        return groups;
     }
     
 }
